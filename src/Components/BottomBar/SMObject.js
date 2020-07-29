b56a0476db49d9ad4e55d8c580cb3d3a77eb24a5
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-07-29 15:11:45 -0400

    made logo bigger and adjusted css on object icon in SM

--- src/Components/BottomBar/SMObject.js
+++ src/Components/BottomBar/SMObject.js
@@ -5,7 +5,7 @@ const SMObject = (props)=>{
 
     return(
         <div className='objectIcon' onClick={()=>props.setActive(props.obj)}>
-            <img src="https://img.icons8.com/dotty/80/000000/3d-scale.png" alt='object icon'/>
+            <img src={require('./obj.png')} alt='object icon'/>
             <p>{props.obj.type==='Mesh' ? props.obj.geometry.type : props.obj.type}</p>
         </div>
     )
