433874cedc7e4d0b5936b21d277cc7bf4599be24
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-10-16 12:05:28 -0400

    added screenshot function

---
+++ src/Functions/screenshot.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three'
+
+const screenshot=(gl,scene,camera,fd)=>{
+    gl.render(scene,camera)
+    gl.toneMapping = THREE.ACESFilmicToneMapping
+    gl.toneMappingExposure = 0.6
+    gl.outputEncoding = THREE.sRGBEncoding
+    gl.preserveDrawingBuffer = true
+    return new Promise((resolve,reject)=>{
+        gl.domElement.toBlob(
+            (blob)=>{
+               resolve(blob)
+            },
+            'image/jpg',
+            1.0
+        )
+    })
+}
+
+export {screenshot}
